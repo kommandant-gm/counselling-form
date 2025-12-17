f5896e4bf148a1ec5451f551d76a73da0c0d4a6b
kommandant-gm <49630340+kommandant-gm@users.noreply.github.com>
2025-12-17 12:07:14 +0800

    name header

--- resources/views/booking-form.blade.php
+++ resources/views/booking-form.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Counselling Session Registration</title>
+    <title>Dayang Counselling Session Registration</title>
     
     <script src="https://cdn.tailwindcss.com"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.15.0/dist/cdn.min.js"></script>
